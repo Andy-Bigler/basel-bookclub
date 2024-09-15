8fb6d61967e96f1077b25ad35939e2a7bc5ad52d
Bigler Andy <andy.bigler@besonet.ch>
2024-09-15 23:49:33 +0200

    ci: dockerize

--- ui/src/routes/+page.server.ts
+++ ui/src/routes/+page.server.ts
@@ -1,7 +1,7 @@
 import PocketBase from 'pocketbase';
 
 export async function load() {
-	const pb = new PocketBase('http://127.0.0.1:8090');
+	const pb = new PocketBase('http://localhost:8090');
 	return {
 		test: await pb.collection('test').getFullList({
 			sort: '-created',
