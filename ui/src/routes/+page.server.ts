c36c8b740e3078412a59a78717aad93b33e2e363
Bigler Andy <andy.bigler@besonet.ch>
2024-10-14 22:40:58 +0200

    chore: tmp hardcode endpoint

--- ui/src/routes/+page.server.ts
+++ ui/src/routes/+page.server.ts
@@ -1,7 +1,7 @@
 import PocketBase from 'pocketbase';
 
 export async function load() {
-	const pb = new PocketBase('http://localhost:8090');
+	const pb = new PocketBase('https://baselbookclubinternational.com/db'); // ToDo dynamic
 	return {
 		test: await pb.collection('test').getFullList({
 			sort: '-created',
